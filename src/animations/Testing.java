48029046bcddaff7720d918afdf434c677dfbc6d
Jonathan <Jonathan@LAPTOP-TTR2R7KQ>
2022-10-17 11:36:43 -0400

    Just another push

---
+++ src/animations/Testing.java
@@ -0,0 +1,10 @@
+package animations;
+
+public class Testing {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(Colour.BLACK + Colour.WHITE_BACKGROUND + "▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄\n▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄\n");
+	}
+
+}
