efc7505c8333a956e70cb957dcab0677ae1876e5
33032pol <33032pol@RESU-216-21.wcdsb.ca>
2022-10-20 12:48:01 -0400

    just a push

--- src/animations/Eyes_Animation.java
+++ src/animations/Eyes_Animation.java
@@ -1,10 +1,11 @@
+
 package animations;
 
 public class Eyes_Animation {
 
 	public static void main(String[] args) throws InterruptedException 
 	{
-		
+
 		System.out.print(Colour.BLACK +  "\r\n"
 				+ "\r\n"
 				+ "\r\n"
@@ -24,7 +25,7 @@ public class Eyes_Animation {
 				+ "		       ██████████		██████████\r\n"
 				+ "		       ██████████		██████████\r\n"
 				+ "		       ▀████████▀		▀████████▀\n");
-		
+
 		Thread.sleep(1000);
 		System.out.print(Colour.BLACK +  "\r\n"
 				+ "\r\n"
@@ -44,10 +45,10 @@ public class Eyes_Animation {
 				+  "      		       ▄████████▄		▄████████▄\r\n"
 				+ "		       ██████████		██████████\n"
 				+ "		       ██████████		██████████");
-		
+
 		System.out.print(Colour.WHITE +  "\r\n"
 				+ "		       ▀████████▀		▀████████▀\n");
-		
+
 		Thread.sleep(100);
 		System.out.print(Colour.BLACK +  "\r\n"
 				+ "\r\n"
@@ -66,11 +67,11 @@ public class Eyes_Animation {
 				+ "\r\n"
 				+  "      		       ▄████████▄		▄████████▄\r\n"
 				+ "		       ██████████		██████████\n");
-		
+
 		System.out.print(Colour.WHITE + "		 	       ██████" + Colour.CYAN + "██" + Colour.WHITE + "██               ██████" + Colour.CYAN + "██" + Colour.WHITE + "██\n");
-		
+
 		System.out.print(Colour.WHITE + "		               ▀████████▀               ▀████████▀\n");
-		
+
 		//Look Right
 		Thread.sleep(100);
 		System.out.print(Colour.BLACK +  "\r\n"
@@ -89,13 +90,13 @@ public class Eyes_Animation {
 				+ "\r\n"
 				+ "\r\n"
 				+  "      		       ▄████████▄		▄████████▄\r\n");
-		                                                                
+
 		System.out.print(Colour.WHITE + "                       ██████████               ██████████\n" + "                       ██████" + Colour.CYAN + "██" + Colour.WHITE + "██               ██████" + Colour.CYAN + "██" + Colour.WHITE + "██\n");
-		
+
 		System.out.print(Colour.WHITE
 				+ "		               ▀████████▀               ▀████████▀\n");
-		
-		
+
+
 		//Look Center
 		Thread.sleep(750);
 		System.out.print(Colour.BLACK +  "\r\n"
@@ -114,12 +115,12 @@ public class Eyes_Animation {
 				+ "\r\n"
 				+ "\r\n"
 				+  "      		       ▄████████▄		▄████████▄\r\n");
-		                                                                
+
 		System.out.print(Colour.WHITE + "                       ██████████               ██████████\n" + "                       ████" + Colour.CYAN + "██" + Colour.WHITE + "████               ████" + Colour.CYAN + "██" + Colour.WHITE + "████\n");
-		
+
 		System.out.print(Colour.WHITE
 				+ "		               ▀████████▀               ▀████████▀\n");
-		
+
 		//Look Left
 		Thread.sleep(100);
 		System.out.print(Colour.BLACK +  "\r\n"
@@ -138,12 +139,12 @@ public class Eyes_Animation {
 				+ "\r\n"
 				+ "\r\n"
 				+  "      		       ▄████████▄		▄████████▄\r\n");
-		                                                                
+
 		System.out.print(Colour.WHITE + "                       ██████████               ██████████\n" + "                       ██" + Colour.CYAN + "██" + Colour.WHITE + "██████               ██" + Colour.CYAN + "██" + Colour.WHITE + "██████\n");
-		
+
 		System.out.print(Colour.WHITE
 				+ "		               ▀████████▀               ▀████████▀\n");
-		
+
 		//Look Left and Up
 		Thread.sleep(2000);
 		System.out.print(Colour.BLACK +  "\r\n"
@@ -162,12 +163,12 @@ public class Eyes_Animation {
 				+ "\r\n"
 				+ "\r\n"
 				+  "      		       ▄████████▄		▄████████▄\r\n");
-		                                                                
+
 		System.out.print(Colour.WHITE + "                       ██" + Colour.CYAN + "██" + Colour.WHITE + "██████               ██" + Colour.CYAN + "██" + Colour.WHITE + "██████\n" + "                       ██████████               ██████████\n");
-		
+
 		System.out.print(Colour.WHITE
 				+ "		               ▀████████▀               ▀████████▀\n");
-		
+
 		//Blink
 		Thread.sleep(875);
 		System.out.print(Colour.BLACK +  "\r\n"
@@ -189,7 +190,7 @@ public class Eyes_Animation {
 				+ "		       ██████████		██████████\r\n"
 				+ "		       ██████████		██████████\r\n"
 				+ "		       ▀████████▀		▀████████▀\n");
-		
+
 		//Look Left and Up
 		Thread.sleep(101);
 		System.out.print(Colour.BLACK +  "\r\n"
@@ -208,12 +209,12 @@ public class Eyes_Animation {
 				+ "\r\n"
 				+ "\r\n"
 				+  "      		       ▄████████▄		▄████████▄\r\n");
-		                                                                
+
 		System.out.print(Colour.WHITE + "                       ██" + Colour.CYAN + "██" + Colour.WHITE + "██████               ██" + Colour.CYAN + "██" + Colour.WHITE + "██████\n" + "                       ██████████               ██████████\n");
-		
+
 		System.out.print(Colour.WHITE
 				+ "		               ▀████████▀               ▀████████▀\n");
-		
+
 		//Look Center
 		Thread.sleep(975);
 		System.out.print(Colour.BLACK +  "\r\n"
@@ -232,12 +233,12 @@ public class Eyes_Animation {
 				+ "\r\n"
 				+ "\r\n"
 				+  "      		       ▄████████▄		▄████████▄\r\n");
-		                                                                
-		System.out.print(Colour.WHITE + "                       ██████████               ██████████\n" + "                       ██████" + Colour.CYAN + "██" + Colour.WHITE + "██               ██" + Colour.CYAN + "██" + Colour.WHITE + "██████\n");
-		
+
+		System.out.print(Colour.WHITE + "                       ██████████               ██████████\n" + "                       ██████" + Colour.BLACK + "█" + Colour.RED + Colour.BLACK_BACKGROUND + "▄" + Colour.WHITE + "██               ██" + Colour.RED + Colour.BLACK_BACKGROUND +  "▄" + Colour.BLACK + "█" + Colour.WHITE + "██████\n");
+
 		System.out.print(Colour.WHITE
 				+ "		               ▀████████▀               ▀████████▀\n");
-		
+
 	}
 
 }
---
+++ src/animations/lol.java
@@ -0,0 +1,20 @@
+package animations;
+
+public class lol {
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		while(true)
+		{
+			double Randnum  = (double)(Math.random()*(1000000000-(-1000000000)+(-1000000000))) + (-1000000000);
+			System.out.println(Randnum);
+			double RandCheck  = (double)(Math.random()*(1000000000-(-1000000000)+(-1000000000))) + (-1000000000);
+			if(RandCheck == Randnum) 
+			{
+				break;
+			}
+			//Thread.sleep(100);
+		}
+	}
+
+}
