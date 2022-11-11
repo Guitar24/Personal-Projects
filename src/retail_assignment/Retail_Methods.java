bfc258d2980e365d4e7eab967a293d1a97159c9a
33032pol <33032pol@RESU-216-21.wcdsb.ca>
2022-11-11 12:48:26 -0500

    Just a push

---
+++ src/retail_assignment/Retail_Methods.java
@@ -0,0 +1,20 @@
+package retail_assignment;
+
+public class Retail_Methods
+{
+	public static
+	
+	public static String Menu()
+	{
+		System.out.println("█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n"
+						 + "█  A - Add Student          █\n"
+						 + "█  R - Remove Student       █\n"
+						 + "█  U - Update Student Mark  █\n"
+						 + "█  V - View Class Info      █\n"
+						 + "█  E - Exit                 █\n"
+						 + "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█");
+		String menuInput = 
+		
+		return null;
+	}
+}
